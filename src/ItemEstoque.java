a80e3db5d2ffabc0bc5d4600dc7fad7be4dbdfcf
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-03-30 14:18:40 -0300

    Criação da classe ItemEstoque add quantidade

---
+++ src/ItemEstoque.java
@@ -0,0 +1,39 @@
+public class ItemEstoque {
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemEstoque(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    @Override
+    public String toString() {
+        return """
+           +-----------------------------------+
+           | ITEM DO ESTOQUE                   |
+           +-----------------------------------+
+           | Produto: %-25s |
+           | Preço:   R$ %-20.2f |
+           | Qtd:     %-20d |
+           +-----------------------------------+
+           | VALOR EM ESTOQUE: R$ %-20.2f |
+           +-----------------------------------+
+           """.formatted(produto.getNome(),
+                produto.getPreco(),
+                this.quantidade,
+                (produto.getPreco() * this.quantidade));
+    }
+}
