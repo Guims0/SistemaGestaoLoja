941462ce331677fa0a4a069af5e3438454517f0b
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-03-31 20:17:45 -0300

    Criaçao da chamada do sistema Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Estoque estoque = new Estoque();
+        MenuAdmin menuAdmin = new MenuAdmin(scanner,estoque);
+        MenuCliente menuCliente = new MenuCliente(scanner,estoque);
 
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.print("""
+                    
+                    === E-COMMERCE ===
+                    1. Acesso Administrador (Estoque)
+                    2. Acesso Cliente (Loja e Carrinho)
+                    0. Desligar Sistema
+                    ====================================
+                    Escolha sua opção:""");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if (opcao == 1) {
+                menuAdmin.iniciar();
+
+            } else if (opcao == 2) {
+
+                menuCliente.iniciar();
+
+            } else if (opcao == 0) {
+                System.out.println("Desligando o sistema. Até logo.");
+
+            } else {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
     }
 }
